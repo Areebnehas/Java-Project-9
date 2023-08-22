688ec4893ea027d797ab98e001298bfa3d2680f7
Areeb <muhammadnehas1@gmail.com>
2023-08-22 18:54:12 -0500

    final commit

---
+++ JavaAssignment9/src/main/java/org/example/MainClass1.java
@@ -0,0 +1,17 @@
+package org.example;
+public class MainClass1 {
+        public static void main(String[] args) {
+
+
+            OverloadedMethods calculator = new OverloadedMethods();
+
+                System.out.println("Assignment was hard")
+
+        System.out.println("Sum of two integers is: " + calculator.add(3, 10));
+
+               System.out.println("Sum of three integers is: " + calculator.add(42, 10, 15));
+            System.out.println("Sum of two doubles is: " + calculator.add(9.3, 32.7));
+        }
+    }
+
+
